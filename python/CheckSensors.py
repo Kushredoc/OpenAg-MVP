ba89cb006e5c455c9c05553c762d41dfade9ea1e
webbhm <webb.howard@gmail.com>
2017-06-01 18:29:24 -0400

    Delete CheckSensors.py
    No longer used, logic moved to logSensors

--- python/CheckSensors.py
+++
@@ -1,6 +0,0 @@
-#Check sensors and log to file
-from si7021 import getTempC, getHumidity
-from logData import logData
-
-logData("Temperature", "{:10.1f}".format(getTempC()), '')
-logData("Humidity", "{:10.1f}".format(getHumidity()), '')
