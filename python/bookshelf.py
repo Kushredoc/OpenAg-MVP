2cf6234a7b1e8afc32e1a79a21279da106b21ea2
webbhm <webb.howard@gmail.com>
2017-06-01 15:07:10 -0400

    Add files via upload

--- python/bookshelf.py
+++ python/bookshelf.py
@@ -7,14 +7,14 @@ import shelve
 
 def putOnShelf(key, value):
     "Persist a variable to storage"
-    filename = "/home/pi/python/bookshelf"
+    filename = "/home/pi/python/bookshelf.db"
     d = shelve.open(filename, protocol= 2)
     d[key] = value
     d.close()
 
 def takeOffShelf(key):
     "Retreive a variable from storage"
-    filename = "/home/pi/python/bookshelf"
+    filename = "/home/pi/python/bookshelf.db"
     d = shelve.open(filename, protocol= 2)
     value = d[key]
     return value
