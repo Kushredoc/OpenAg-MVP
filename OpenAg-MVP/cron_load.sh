658d293490504c90a0c3361b017a130d1203a37c
webbhm <webb.howard@gmail.com>
2017-06-01 20:36:43 -0400

    Add files via upload

--- OpenAg-MVP/cron_load.sh
+++ OpenAg-MVP/cron_load.sh
@@ -4,7 +4,5 @@ crontab -l > mycron
 echo "/20 * * * * python /home/pi/python/logSensors.py"
 #add temperature checking every minute
 echo "/1 * * * * python /home/pi/python/thermostat.py"
-#add light checking every five minutes
-echo "/5 * * * * python /home/pi/python/ambientLight.py"
-#take photo every 4 hours
-echo "* /4 * * * /home/pi/Documents/OpenAg-MVP/webcam.sh"
+#take photo every 4 hours when lights are on
+echo "6,12,4,8 * * * /home/pi/Documents/OpenAg-MVP/webcam.sh"
