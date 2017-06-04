615abad8740c1c2632dc0def80a3a76daa74206d
webbhm <webb.howard@gmail.com>
2017-06-04 14:30:33 -0400

    Add files via upload
    Remove light references

--- python/setup.py
+++ python/setup.py
@@ -3,7 +3,5 @@
 from bookshelf import takeOffShelf, putOnShelf
 
 putOnShelf("targetTemp", 24)
-putOnShelf("lightOn", "0600")
-putOnShelf("lightOff", "2230")
 putOnShelf("priorFanOn", True)
-putOnShelf("priorLightOn", True)
+
