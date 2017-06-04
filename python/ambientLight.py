e1f6f99105606c342a579f7191dca4f51699541c
webbhm <webb.howard@gmail.com>
2017-06-04 14:21:35 -0400

    Delete ambientLight.py

--- python/ambientLight.py
+++
@@ -1,9 +0,0 @@
-#ambientLight.py
-#Checks the light via a photo cell and determines of the lights are on or off
-#If the is a change, log the chanage
-#External module imports
-
-from checkLight import *
-
-#cron job will check lights
-checkLight()        
