3d8d836f05e99a38ca94911c76ccdb25dddee33b
webbhm <webb.howard@gmail.com>
2017-06-04 14:23:06 -0400

    Add files via upload
    Simplify light control with cron

---
+++ python/setLightOn.py
@@ -0,0 +1,21 @@
+#Light Control
+#Controls the turning on and turning off of lights
+#Lights are wired into Relay #4 (Pin 29)
+
+import RPi.GPIO as GPIO
+from logData import logData
+
+
+def setLightOn():
+    "Check the time and determine if the lights need to be changed"
+    lightPin = 29
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BOARD)
+    print ("Turn lights on")
+    GPIO.setup(lightPin, GPIO.OUT)
+    GPIO.output(lightPin, GPIO.HIGH)
+    logData("LightChange", "On", '')
+    
+            
+    
+
