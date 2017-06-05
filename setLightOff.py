4ff472979e92be9cf32790cd2c8ee7e852b35a04
webbhm <webb.howard@gmail.com>
2017-06-05 08:41:41 -0400

    Add files via upload
    added fcall so would run from cron

---
+++ setLightOff.py
@@ -0,0 +1,23 @@
+#Light Control
+#Controls the turning on and turning off of lights
+#Lights are wired into Relay #4 (Pin 29)
+
+import RPi.GPIO as GPIO
+from logData import logData
+
+
+def setLightOff():
+    "Check the time and determine if the lights need to be changed"
+    lightPin = 29
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BOARD)
+    print ("Turn lights off")
+    GPIO.setup(lightPin, GPIO.OUT)
+    GPIO.output(lightPin, GPIO.LOW)
+    logData("LightChange", "Off", '')
+
+setLightOff()
+    
+            
+    
+
