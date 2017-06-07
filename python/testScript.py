4a64b1b9b15886d8d9ddff48c8cafbe8d83b5995
webbhm <webb.howard@gmail.com>
2017-06-06 21:07:23 -0400

    Add files via upload
    Update to latest changes

--- python/testScript.py
+++ python/testScript.py
@@ -1,9 +1,11 @@
 #A script to test the functions of the OpenAg-MVP routines
 #Run this to validate code and that things are working correctly
 
-from checkLight import *
 from si7021 import *
 from bookshelf import *
+from thermostat import *
+from setLightOn import *
+from setLightOff import *
 
 try:
     print ("Execute getTempC")
@@ -18,16 +20,33 @@ except (RuntimeError):
     print ("Failure to get Humidity")
 
 try:
-    print ("Check Lights")    
-    print (checkLight())
-except (RuntimeError):
-    print ("Failure in checkLight")
+    print ("Get from Bookshelf")    
+    print (takeOffShelf("targetTemp"))
+except (RuntimeError):    
+    print ("Failure to get value for key")
 
 try:
-    print ("Get from Bookshelf")    
-    print (takeOffShelf("priorLightOn"))
-except (RuntimeError):    
-    print ("Failure to get value for key")            
-    
+    print ("Check Thermostat High")
+    print (adjustThermostat(75))
+except (RuntimeError):
+    print ("Failure adjusting thermostat: High")
+
+try:
+    print ("Check Thermostat Low")
+    print (adjustThermostat(15))
+except (RuntimeError):
+    print ("Failure adjusting thermostat: Low")
+
+try:
+    print ("Turn Light On")
+    print (setLightOn())
+except (RuntimeError):
+    print ("Failure to turn light ON")    
+   
+try:
+    print ("Turn Light OFF")
+    print (setLightOff())
+except (RuntimeError):
+    print ("Failure to turn light OFF")    
 
 
