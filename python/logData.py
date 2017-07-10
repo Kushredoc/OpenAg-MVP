0f9cf4f52cc308934428884e514bdd6e006ade81
webbhm <webb.howard@gmail.com>
2017-07-10 15:45:08 -0500

    Made compatible with Python 3
    Changed print to functions.

--- python/logData.py
+++ python/logData.py
@@ -7,10 +7,10 @@ import json
 def logData(name, status, attribute, value, comment):
     timestamp = '{:%Y-%m-%d %H:%M:%S}'.format(datetime.utcnow())
     logFile(timestamp, name, status, attribute, value, comment)
-#    logDB(timestamp, name, status, attribute, value, comment)
+    logDB(timestamp, name, status, attribute, value, comment)
     
 def logFile(timestamp, name, status, attribute, value, comment):
-    f = open('/home/pi/Documents/OpenAg-MVP/data.txt', 'a')
+    f = open('/home/pi/MVP_Brain/data.txt', 'a')
     s= timestamp + ", " + name + ", " + status + ", " + attribute + ", " + value + "," + comment + "\n"
     print(s)
     f.write(s)
@@ -24,10 +24,10 @@ def logDB(timestamp, name, status, attribute, value, comment):
             'value' : value,
             'comment' : comment}
     json_data = json.dumps(log_record)
-    print json.dumps(log_record, indent=4, sort_keys=True)
+    print(json.dumps(log_record, indent=4, sort_keys=True))
     headers = {'content-type': 'application/json'}
     r = requests.post('http://localhost:5984/mvp_sensor_data', data = json_data, headers=headers)
-    print r.json()
+    print(r.json())
 
 #Uncomment to test this function
-#logData('si7021-top', 'Success', 'temperature', '27', '')    
+#logData(_si7021, _Success, _temperature, '27', '')    
