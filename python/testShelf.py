7f559703497de4aea55356a81fe86a782bfb06d9
webbhm <webb.howard@gmail.com>
2017-07-28 12:12:38 -0400

    Add files via upload

---
+++ python/testShelf.py
@@ -0,0 +1,7 @@
+# Test retreival of variables
+
+from bookshelf import *
+
+print("***Test variable storage***")
+print("Target Temp: %s" %str(takeOffShelf('targetTemp')))
+print("Prior Fan On: %s" %str(takeOffShelf('priorFanOn')))
