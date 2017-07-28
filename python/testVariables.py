c8ab284e2acaee3e34e2b31e0e049aa19ef6117d
webbhm <webb.howard@gmail.com>
2017-07-28 12:11:54 -0400

    Delete testVariables.py

--- python/testVariables.py
+++
@@ -1,7 +0,0 @@
-# Test retreival of variables
-
-from persistVariable import *
-
-print("***Test variable storage***")
-print("Target Temp: %s" %str(getVariable('targetTemp')))
-print("Prior Fan On: %s" %str(getVariable('priorFanOn')))
